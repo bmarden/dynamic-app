f1f76c8a8f4b1a4880a189b6bf672ea9fc0628a9
Ben Marden <ben.marden@amenitieshealth.com>
2023-06-21 00:07:58 -0700

    add custom expo module

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Application from 'expo-application';
+import { hello } from './modules/test-module';
 
 export default function App() {
   return (
