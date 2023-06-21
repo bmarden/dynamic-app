afa9f3bd470fc15f2c62998fa78502c2c6186db6
Ben Marden <ben.marden@amenitieshealth.com>
2023-06-21 13:15:51 -0700

    Commit missing ios/android folders in module directory

---
+++ modules/expo-web-view/android/src/main/java/expo/modules/webview/ExpoWebView.kt
@@ -0,0 +1,28 @@
+package expo.modules.webview
+
+import android.content.Context
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+import expo.modules.kotlin.viewevent.EventDispatcher
+
+class ExpoWebView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
+  private val onLoad by EventDispatcher()
+
+  internal val webView = WebView(context).also {
+    it.layoutParams = LayoutParams(
+      LayoutParams.MATCH_PARENT, 
+      LayoutParams.MATCH_PARENT
+    )
+
+    it.webViewClient = object : WebViewClient() {
+      override fun onPageFinished(view: WebView?, url: String?) {
+        onLoad(mapOf("url" to url))
+      }
+    }
+    addView(it)
+
+    it.loadUrl("https://docs.expo.dev/modules/")
+  } 
+}
